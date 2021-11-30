1d2978874075cf4374d4998acb82c0c5aed97121
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-30 15:32:36 -0700

    unit tests for edge cases

--- code.js
+++ code.js
@@ -3,6 +3,16 @@ function helloWorld() {
     return "Hello, World!";
 }
 
+// sayHello function
+const obj ={}; // empty object for testing
+const foo = function(){}; // empty function for testing
+
 function sayHello(name){
-    return "Hello, " + name + "!";
+    if(name === true || name === false || arguments.length === 0 || name === null || name === ''){
+        return "Hello, Stranger!";
+    } else if (typeof name === "number" || typeof name === "object" || typeof name === "function"){
+        return "Hello, World!";
+    } else {
+        return "Hello, " + name + "!";
+    }
 }
--- tests.js
+++ tests.js
@@ -14,7 +14,7 @@ describe('helloWorld', function() {
     });
 });
 
-
+// Unit Tests for sayHello function
 describe('sayHello', function() {
     it("should be a defined function", function(){
        expect(typeof sayHello).toBe("function");
@@ -30,6 +30,30 @@ describe('sayHello', function() {
     });
     it("sayHello('Pat') should return 'Hello, Pat!'", function(){
         expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it("sayHello() should return 'Hello, Stranger!'", function(){
+        expect(sayHello()).toBe('Hello, Stranger!');
+    });
+    it("sayHello(true) should return 'Hello, Stranger!'", function(){
+        expect(sayHello(true)).toBe('Hello, Stranger!');
+    });
+    it("sayHello(false) should return 'Hello, Stranger!'", function(){
+        expect(sayHello(false)).toBe('Hello, Stranger!');
+    });
+    it("sayHello(null) should return 'Hello, Stranger!'", function(){
+        expect(sayHello(null)).toBe('Hello, Stranger!');
+    });
+    it("sayHello('') should return 'Hello, Stranger!'", function(){
+        expect(sayHello('')).toBe('Hello, Stranger!');
+    });
+    it("sayHello(2.3) should return 'Hello, World!'", function(){
+        expect(sayHello(2.3)).toBe('Hello, World!');
+    });
+    it("sayHello(obj), obj being an object type, should return 'Hello, World!'", function(){
+        expect(sayHello(obj)).toBe("Hello, World!");
+    });
+    it("sayHello(foo), foo being a function, should return 'Hello, World!'", function(){
+        expect(sayHello(foo)).toBe("Hello, World!");
     });
 });
 
